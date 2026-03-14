1d86e2c7edc9e1e66bee68f42465f15ff1af435b
Akilan K <ponakilan25@gmail.com>
2026-03-14 16:10:06 +0000

    Return image ID

--- cbir/dataset.py
+++ cbir/dataset.py
@@ -17,6 +17,7 @@ class CbirDataset(Dataset):
         self.transform = transform
         if not transform:
             self.transform = transform = transforms.Compose([
+                transforms.Resize((512, 512)),
                 transforms.ConvertImageDtype(torch.float32),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
@@ -24,7 +25,9 @@ class CbirDataset(Dataset):
     def __len__(self):
         return len(os.listdir(self.root_dir))
     
-    def __getitem__(self, index: int) -> torch.Tensor:
-        image_path = self.image_files[index]
+    def __getitem__(self, index: int) -> tuple[torch.Tensor, str]:
+        filename = self.image_files[index]
+        image_path = os.path.join(self.root_dir, filename)
         image = self.transform(io.read_image(image_path)) # type: ignore
-        return image
+        image_id = filename.split('.')[0]
+        return image, image_id
